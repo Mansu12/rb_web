0ec4cc04eb8b1188130e4dbbce7cf6cc2bcc4eb5
joeljangam <joeljangam146@gmail.com>
2018-02-01 11:11:29 +0530

    created sbi blade

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -193,7 +193,7 @@ public function  change_password(Request $req){
   public function applyonline(Request $req){
     
     $request = $req->all();
-      //print_r($request);exit();
+      // print_r($request);exit();
     // $app = $request['appid'];
     $quote = $request['qoutid'];
     $bank = $request['BankId'];
@@ -204,6 +204,7 @@ public function  change_password(Request $req){
     }
      if(isset($request['loaninterest'])){
       $loaninterest = $request['loaninterest'];
+       // print_r($loaninterest);exit();
     }else{
       $loaninterest = "";
     }
